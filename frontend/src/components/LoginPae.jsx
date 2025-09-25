01dd04e0ebf06eaeabcbf2d5bd2dfa4e1a9319db
Abhishek Jain <150254902+Abhijain01@users.noreply.github.com>
2025-09-25 07:39:46 +0530

    Update LoginPae.jsx

--- frontend/src/components/LoginPae.jsx
+++ frontend/src/components/LoginPae.jsx
@@ -31,7 +31,7 @@ const LoginPage = () => {
     }
 
     try {
-      const res = await fetch("http://127.0.0.1:5000/send-otp", {
+      const res = await fetch("https://ai-based-agriculture-query-assistant.onrender.com/send-otp", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ phone }),
